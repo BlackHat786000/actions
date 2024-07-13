e6f18cb86933276c20be3dff30eb1b47e50af78a
Udit Yadav <36297285+BlackHat786000@users.noreply.github.com>
2024-07-13 14:15:32 +0530

    Update index.js

--- github-actions/set-async-job-status/index.js
+++ github-actions/set-async-job-status/index.js
@@ -48,7 +48,35 @@ if (authentication && authentication.toUpperCase() === 'SASL/PLAIN') {
     username: sasl_username,
     password: sasl_password,
   };
-  kafkaConfig.ssl = false;
+  kafkaConfig.ssl = true;
+  kafkaConfig.ssl = {
+    rejectUnauthorized: true,
+    ca: [
+      `-----BEGIN CERTIFICATE-----
+      MIID5TCCAs2gAwIBAgIUdj0KhB6OfYslmL0agKHNiDJgkQ0wDQYJKoZIhvcNAQEL
+      BQAwgYExCzAJBgNVBAYTAkFVMRMwEQYDVQQIDApTb21lLVN0YXRlMSEwHwYDVQQK
+      DBhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQxOjA4BgNVBAMMMWVjMi0zLTExMC0z
+      Ny0xNzAuYXAtc291dGgtMS5jb21wdXRlLmFtYXpvbmF3cy5jb20wHhcNMjQwNzEz
+      MDUzMDU4WhcNMjUwNzEzMDUzMDU4WjCBgTELMAkGA1UEBhMCQVUxEzARBgNVBAgM
+      ClNvbWUtU3RhdGUxITAfBgNVBAoMGEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZDE6
+      MDgGA1UEAwwxZWMyLTMtMTEwLTM3LTE3MC5hcC1zb3V0aC0xLmNvbXB1dGUuYW1h
+      em9uYXdzLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKutJp2b
+      FpxmYKARaqHcuUXKXPvqzYMLLIHebb3K+w7CdlW0qPi86UIQgEEZRGA4GWnQ7Mxl
+      iHEFutwn8SWZ0iqb6wirzjIDK7duIkMs3BSgxtSIPdyZdc4Tz+LWBqTIQhh5a3rM
+      EsCsOQb7bgrPZgrUFZWujRwJVAxYXMUaB1IionqjNNoZ10gKjssOotsR45z30MPU
+      zYUYodZLrWhD0pRbtltC0AnwupheakbfWyQlgJI/+keQFIn9vg7SaBcxCjVXiy2s
+      Pc/Xp5y9+NjCCtL4jciAkF7zYUAzYbVPs6Y27Ql9cs+J8PJgoOjIYGHerWW69fGx
+      Oyz0EZtkKGDzu/kCAwEAAaNTMFEwHQYDVR0OBBYEFMpCussWeUpESOM16OwMcBwg
+      wtiGMB8GA1UdIwQYMBaAFMpCussWeUpESOM16OwMcBwgwtiGMA8GA1UdEwEB/wQF
+      MAMBAf8wDQYJKoZIhvcNAQELBQADggEBAKqccJqWyQytALjL4XnjS0c0GK1AeVP5
+      3P+67Jz++8ygvBWVF/7X/mN5wEgYYzJeZGV8dzreZ4XUc0hmgX08JefQmI4h9YW1
+      dD3AgNt2PDlRA4Vu9XidZcIqZm8Fm9YO2He2w7dgl7nl9A+GWHa/kIDqQTKytNLl
+      A5QwCcZ07NDoWU14w9CHdn/kLViV0nQngk1YZ5go7LZ2UAuqfx2Duy3i5Gk1BNqe
+      rMYp6RFWXKnkj3Lm5mJspBiJ1IHjOtWsz7DJ9Emz3nIso9QkZjvFVCKxYWqc7o3j
+      fi2QkmZtZk9yEB+xjXoSFAcd95DpugcWSdFNGnhHG+daEdbFgMlmee4=
+      -----END CERTIFICATE----`
+    ]
+  };
 }
 
 const kafka = new Kafka(kafkaConfig);
